57b7888ef0c2aacd9a388d10df01ecc04d774a70
wangchengni <wangchengni001@gmail.com>
2021-10-22 17:12:33 -0600

    main

---
+++ src/com/skilldistillery/jets/app/JetsApplictaion.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.jets.app;
+
+import java.util.Scanner;
+
+import com.skilldistillery.jets.entities.AirField;
+
+public class JetsApplictaion {
+
+	private AirField airfield;
+	Scanner kb = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
